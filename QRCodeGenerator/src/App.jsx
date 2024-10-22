776c61a1fb515d5bee26b81c024b6369f1040164
chrisRV15 <christopher.d.thomasvillalobos@lions.enc.edu>
2024-10-22 15:45:36 -0600

    Finish Qr code Generator

--- QRCodeGenerator/src/App.jsx
+++ QRCodeGenerator/src/App.jsx
@@ -6,16 +6,37 @@ export default function App(){
   const [url, setUrl] = useState('')
   const [qrcode, setQrcode] =useState ('')
 
+  const generateQR = async () => {
+    try{
+      const response = await QRCode.toDataURL(url, {
+        width: 700,
+        margin: 2
+      })
+      console.log(url)
+      setQrcode(response)
+    }
+    catch (err){
+      console.error(err)
+    }
+  }
+
   return (
     <>
     <div className='App-Container'>
       <div>
         <h1>QR Code Generator</h1>
-        <input type='text' placeholder='https://www.google.com/'></input>
-        <button>Generate</button>
+        <input type='text' 
+        placeholder='https://www.google.com/'
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+        />
+        <button onClick={generateQR}>Generate</button>
+        {qrcode && <>
           <div>
-            <img src=''></img>
+            <img src={qrcode}/>
+            <a href={qrcode} download="QRCode.png">Download</a>
           </div> 
+        </>}
       </div>
     </div>
     </>
